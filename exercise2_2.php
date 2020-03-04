2e64ac4c05605979cfa0f27ce4c1448d33fe2d8e
JiayuDuan16156331 <61173987+JiayuDuan16156331@users.noreply.github.com>
2020-03-04 22:24:32 +0800

    exercise2-2
    my website can't link the database(ps: I have opened  Xampp control

--- exercise2_2.php
+++ exercise2_2.php
@@ -5,18 +5,21 @@ $password = "";
 $dbname = "bts1";
 
 // Create connection
-$conn = mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO genre (genreID, genre) VALUES ('', 'fiction')";
+
+$sql = "INSERT INTO bts_group (Img, Name, Birthday, Description) VALUES ('images/1.jpg','BTS','222','44')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
 $conn->close();
 ?>
