c505a07dead909de07373d32d5270fe6c4e84fc5
JiayuDuan16156331 <61173987+JiayuDuan16156331@users.noreply.github.com>
2020-03-13 16:38:50 +0800

    pokemon

--- week3/apio.php
+++ week3/apio.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>API Showcase</title>
+	<title>pokemon API Showcase</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -9,53 +9,19 @@
 </head>
 <body>
 	<?php
+	  $ctx = stream_context_create(array('http' => array('header' => "Content-Type:application/json")
+	));
+    $poke='1';
+		while ($poke < 10) {
 
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		$pokemon = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$poke, false, $ctx);
+		$pokemon = json_decode($pokemon);
 
-		$query = array(
-			"api-key" => '',
-			"query" => 'limber'
-		);
-
-		curl_setopt($curl, CURLOPT_URL, "https://pokeapi.co"."?".http_build_query($query));
-
-		$poke_search = json_decode(curl_exec($curl));
-
-		$count = 0;
-		echo '<div class="container">
-				<div class="row">';
-					foreach ($poke_search->results as $poke){
-						if ($count == 2){
-							echo "</div><div class='row'>";
-							$count = 0;
-						}
-						echo "<div class='col-12 col-md-6'>
-						<h3>".$poke->name."</h3>";
-						if (empty($poke->pokemon->url) == false){
-							echo "<img src='".$poke->pokemon->url."'";
-
-						}
-						echo "<br><h4>".$poke->name."</h4><br>
-						<p>".$poke->order."</p></br>
-						<a href='".$poke->version_group->url."'>Review</a>
-
-
-
-
-						</div>";
-						$count = $count+1;
-
-
-
-
-					}
-					echo "</div>";
-
-
-
-
-
+						echo '<img src="'.$pokemon->sprites->front_shiny.'"><p>';
+						echo '<h1>'.ucfirst($pokemon->name).'</h1>';
+					  echo '</P>';
+				   	$poke++;
+					};
 	?>
 </body>
 </html>
